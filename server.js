bb6430c9ff86c7485b6515834f1b5b7ecc4eacea
Chutipon Hirankanokkul <chutiponh@hotmail.com>
2019-04-21 10:48:28 +0700

    :rocket: set nom script for deployment

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 
 const app = require('./read/index.js');
 
-app.listen(3000,() => {
-    console.log('Listening on port 3000');
+app.listen(process.env.PORT || 3000,() => {
+    console.log(`Listening on port ${process.env.PORT || 3000}`);
 });
